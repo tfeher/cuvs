b341ab98062fa51227c4cf8e88ef14f143affced
Tamas Bela Feher <tfeher@nvidia.com>
2025-11-14 15:22:03 +0100

    Add CAGRA-HNSW ACE OpenAI 5M example

--- examples/cpp/src/cagra_hnsw_ace_example.cu
+++ examples/cpp/src/cagra_hnsw_ace_example.cu
@@ -19,7 +19,14 @@
 
 #include "common.cuh"
 
-void cagra_build_search_ace(raft::device_resources const& dev_resources,
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib> // for exit
+
+int cagra_build_search_ace(raft::device_resources const& dev_resources,
                             raft::device_matrix_view<const float, int64_t> dataset,
                             raft::device_matrix_view<const float, int64_t> queries)
 {
@@ -45,7 +52,7 @@ void cagra_build_search_ace(raft::device_resources const& dev_resources,
   // available host and GPU memory. The partition size is on average 2 * (n_rows / npartitions) *
   // dim * sizeof(T). 2 is because of the core and augmented vectors. Please account for imbalance
   // in the partition sizes (up to 3x in our tests).
-  ace_params.npartitions = 4;
+  ace_params.npartitions = 10;
   // Set the index quality for the ACE build. Bigger values increase the index quality. At some
   // point, increasing this will no longer improve the quality.
   ace_params.ef_construction = 120;
@@ -55,18 +62,34 @@ void cagra_build_search_ace(raft::device_resources const& dev_resources,
   // Set whether to use disk-based storage for ACE build. When true, enables disk-based operations
   // for memory-efficient graph construction. If not set, the index will be built in memory if the
   // graph fits in host and GPU memory, and on disk otherwise.
-  ace_params.use_disk             = true;
+  ace_params.use_disk  = true;
   index_params.graph_build_params = ace_params;
 
-  // ACE requires the dataset to be on the host
-  auto dataset_host = raft::make_host_matrix<float, int64_t>(dataset.extent(0), dataset.extent(1));
-  raft::copy(dataset_host.data_handle(),
-             dataset.data_handle(),
-             dataset.extent(0) * dataset.extent(1),
-             raft::resource::get_cuda_stream(dev_resources));
-  raft::resource::sync_stream(dev_resources);
-  auto dataset_host_view = raft::make_host_matrix_view<const float, int64_t, raft::row_major>(
-    dataset_host.data_handle(), dataset_host.extent(0), dataset_host.extent(1));
+  // Open dataset in big-ann-benchmarks binary format.
+  int fd = open("openai_5M/base.5M.fbin", O_RDONLY);
+   if (fd == -1) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+  uint32_t shape[2];
+  ssize_t bytesRead = read(fd, shape, 8);
+  if (bytesRead != 8) {
+        perror("Error reading shape");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+  size_t data_size = shape[0] * static_cast<size_t>(shape[1]);
+  std::cout<< "Dataset size " << data_size << std::endl;
+  size_t header_size = sizeof(shape);
+  size_t file_size = data_size * sizeof(float) + header_size;
+  float *dataset_ptr = (float*) mmap(nullptr, file_size, PROT_READ, MAP_SHARED, fd, 0);
+  std::cout << "shape [" << shape[0] <<", " << shape[1]<<"]"<<std::endl;
+  if (dataset_ptr == MAP_FAILED) {
+        perror("Error mmapping the file");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+  auto dataset_host_view = raft::make_host_matrix_view<const float, int64_t, raft::row_major>(dataset_ptr + header_size, shape[0], shape[1]);
 
   std::cout << "Building CAGRA index (search graph)" << std::endl;
   auto index = cagra::build(dev_resources, index_params, dataset_host_view);
@@ -99,18 +122,6 @@ void cagra_build_search_ace(raft::device_resources const& dev_resources,
   hnsw_search_params.ef          = std::max(200, static_cast<int>(topk) * 2);
   hnsw_search_params.num_threads = 1;
 
-  // If the HNSW index is in memory, search directly
-  // std::cout << "HNSW index in memory. Searching..." << std::endl;
-  // hnsw::search(dev_resources,
-  //              hnsw_search_params,
-  //              *hnsw_index,
-  //              queries_host.view(),
-  //              indices_hnsw_host.view(),
-  //              distances_hnsw_host.view());
-
-  // If the HNSW index is stored on disk, deserialize it for searching
-  std::cout << "HNSW index is stored on disk." << std::endl;
-
   // For disk-based indices, the HNSW index file path can be obtained via file_path()
   std::string hnsw_index_path = hnsw_index->file_path();
   std::cout << "HNSW index file location: " << hnsw_index_path << std::endl;
@@ -124,11 +135,11 @@ void cagra_build_search_ace(raft::device_resources const& dev_resources,
 
   std::cout << "Searching HNSW index." << std::endl;
   hnsw::search(dev_resources,
-               hnsw_search_params,
-               *hnsw_index_deserialized,
-               queries_host.view(),
-               indices_hnsw_host.view(),
-               distances_hnsw_host.view());
+                 hnsw_search_params,
+                 *hnsw_index_deserialized,
+                 queries_host.view(),
+                 indices_hnsw_host.view(),
+                 distances_hnsw_host.view());
 
   // Convert HNSW uint64_t indices back to uint32_t for printing
   auto neighbors_host = raft::make_host_matrix<uint32_t, int64_t>(n_queries, topk);
@@ -150,6 +161,7 @@ void cagra_build_search_ace(raft::device_resources const& dev_resources,
   raft::resource::sync_stream(dev_resources);
 
   print_results(dev_resources, neighbors.view(), distances.view());
+  return 0;
 }
 
 int main()
@@ -168,8 +180,8 @@ int main()
   // raft::resource::set_workspace_to_pool_resource(dev_resources, 2 * 1024 * 1024 * 1024ull);
 
   // Create input arrays.
-  int64_t n_samples = 10000;
-  int64_t n_dim     = 90;
+  int64_t n_samples = 1000;
+  int64_t n_dim     = 1536;
   int64_t n_queries = 10;
   auto dataset      = raft::make_device_matrix<float, int64_t>(dev_resources, n_samples, n_dim);
   auto queries      = raft::make_device_matrix<float, int64_t>(dev_resources, n_queries, n_dim);
